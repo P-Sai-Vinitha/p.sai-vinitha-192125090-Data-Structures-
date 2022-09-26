b329f7b68ba0d2d73a0fa73fcde091c0e804e477
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-26 14:10:59 +0530

    Add files via upload

---
+++ fibonacci using recursion.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+int fibonacci(int);
+int main()
+{
+	printf("NAME-pandeti sai vinitha\n");
+	printf("REG_NO:192125090");
+	printf("\n\n");
+	int n,i;
+	printf("number of element in series:");
+	scanf("%d",&n);
+	printf("fibinocci series is:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",fibonacci(i));
+	}
+	getch();
+}
+int fibonacci(int i)
+{
+	if(i==0)
+	{
+		return 0;
+	}
+	else if(i==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return (fibonacci(i-1)+fibonacci(i-2));
+	}
+}
