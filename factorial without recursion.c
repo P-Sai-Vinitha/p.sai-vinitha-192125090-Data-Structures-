92e2a037da5b1d2f46ac4d7b40cd02443bc5c281
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-26 13:05:33 +0530

    Add files via upload

---
+++ factorial without recursion.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("NAME-pandeti sai vinitha\n");
+	printf("REG_NO:192125090");
+	printf("\n\n");
+	int a,fact=1;
+	scanf("%d",&a);
+	for(int i=1;i<=a;i++)
+	{
+		fact=fact*i;
+	}
+	printf("factorial of %d is %d",a,fact);
+	return 0;
+}
