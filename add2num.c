4b767f0078989bef5d20a048e73e090df4860390
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-26 13:03:07 +0530

    Add files via upload

---
+++ add2num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("NAME-pandeti sai vinitha\n");
+	printf("REG_NO:192125090");
+	printf("\n\n");
+	int a,b,c;
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("sum=%d",c);
+	return 0;
+}
