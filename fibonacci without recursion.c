ca802189014f0890738129e72ae2c931cade59cd
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-26 14:20:58 +0530

    Add files via upload

---
+++ fibonacci without recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("NAME-pandeti sai vinitha\n");
+	printf("REG_NO:192125090");
+	printf("\n\n");
+	int nt=0,n1=1,n2=0,r;
+	printf("enter the number:");
+	scanf("%d",&r);
+	for(int i=0;i<r;i++)
+	{
+		printf("%d ",nt);
+		nt=n1+n2;
+		n1=n2;
+		n2=nt;
+	}
+}
