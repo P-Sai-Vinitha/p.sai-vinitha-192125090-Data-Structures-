bde164db6aa154f707b5992ea5b7a17db0247383
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-26 13:03:38 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("NAME-pandeti sai vinitha\n");
+	printf("REG_NO:192125090");
+	printf("\n\n");
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("its even number");
+	}
+	else
+	{
+		printf("its odd number");
+	}
+}
