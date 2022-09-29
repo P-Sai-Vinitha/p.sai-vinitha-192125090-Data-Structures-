aeccfb50326cf2b51b30e0edc83a357f59883b19
P-Sai-Vinitha <113926586+P-Sai-Vinitha@users.noreply.github.com>
2022-09-29 11:44:22 +0530

    Update queue operations.c

--- queue operations.c
+++ queue operations.c
@@ -85,3 +85,32 @@ void display()
         printf("\n");
     }
 } 
+
+SAMPLE OUTPUT:
+NAME-pandeti sai vinitha
+REG_NO:192125090
+
+1.Insert element to queue
+2.Delete element from queue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 1
+Inset the element in queue : 3
+1.Insert element to queue
+2.Delete element from queue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 2
+Element deleted from queue is : 3
+1.Insert element to queue
+2.Delete element from queue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 3
+Queue is :
+
+1.Insert element to queue
+2.Delete element from queue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 4
